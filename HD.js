634bc38e95c9cde257f5bee5620407f93b61cc79
nq134 <nhuquynh.1304@gmail.com>
2023-06-21 21:22:14 +0700

    comment

---
+++ HD.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+      <div className="Header">
+        <h1>Hi everybody </h1>
+        <nav className="Nav">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        </nav>
+      </div>
+    )
+  };
+  
+  export default Header;
