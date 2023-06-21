2309e950c64ba162f5d27c547f24478a08e28676
QuynhVu <112941153+nq134@users.noreply.github.com>
2023-06-21 22:51:38 +0700

    Add files via upload

---
+++ my/src/App.js
@@ -0,0 +1,16 @@
+import './L2.css';
+import Header from './HD';
+import Content from './CT';
+import Footer from './FT';
+
+
+  function App() {
+    return (
+      <div className="App">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
+export default App;
